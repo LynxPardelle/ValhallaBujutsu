22155912e9df023e6641e4ab2d5c54268ec765ac
Lynx Pardelle <lynxpardelle@lynxpardelle.com>
2021-07-24 03:18:13 +0530

    navbar_magic

--- assets/js/jesus.js
+++ assets/js/jesus.js
@@ -1,1 +1,25 @@
 'use strict'
+
+function headerNavReady() {
+    var nav = document.querySelector("#navBarHeader");
+    if(window.innerWidth > 576) {
+        window.addEventListener("scroll", () => {
+            if (window.pageYOffset > 48) {
+                nav.classList.remove('w-75vw');
+                nav.classList.remove('mt-5');
+                nav.classList.add('w-100vw');
+                // console.log(nav);
+            } else {
+                nav.classList.remove('w-100vw');
+                nav.classList.add('w-75vw');
+                nav.classList.add('mt-5');
+            } 
+        });
+    } else {
+        nav.classList.remove('w-75vw');
+        nav.classList.remove('mt-5');
+        nav.classList.add('w-100vw');
+    }
+
+}
+    
